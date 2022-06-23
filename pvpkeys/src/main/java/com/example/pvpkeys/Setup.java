abc68759e920b854b70d3f8e72d4ff7f326ce5cc
Ethan Vann <Superevevev@gmail.com>
2022-06-23 17:47:25 -0400

    hotfix

--- pvpkeys/src/main/java/com/example/pvpkeys/Setup.java
+++ pvpkeys/src/main/java/com/example/pvpkeys/Setup.java
@@ -9,17 +9,19 @@ public class Setup {
     boolean enabled;
     String[] itemTriggers;
 
-    public Setup(String name, Keybind keybind, String[] commands, boolean enabled) {
+    public Setup(String name, Keybind keybind, String[] commands, boolean enabled, String[] itemTriggers) {
         this.name = name;
         this.keybind = keybind;
         this.commands = commands;
         this.enabled = enabled;
+        this.itemTriggers = itemTriggers;
     }
 
     public Setup(String name) {
         this.name = name;
         this.keybind = Keybind.NOT_SET;
         this.commands = new String[]{""};
+        this.itemTriggers = new String[]{""};
         this.enabled = true;
     }
 }
